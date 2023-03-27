076dcf78a3c397e660ab5d9783c418ce8620efd5
Barthezzone1 <s219117@student.uek.krakow.pl>
2023-03-27 17:07:04 +0200

    pwa

---
+++ sw.js
@@ -0,0 +1,26 @@
+const PWA = "PWA"
+const GHPATH = '/pwa';
+const assets = [
+  `${GHPATH}/`,
+  `${GHPATH}/index.html`,
+  `${GHPATH}/map.html`,
+  `${GHPATH}/sos.html`,
+  `${GHPATH}/sos.js`,
+]
+
+
+self.addEventListener("install", installEvent => {
+  installEvent.waitUntil(
+    caches.open(PWA).then(cache => {
+      cache.addAll(assets)
+    })
+  )
+})
+
+self.addEventListener("fetch", fetchEvent => {
+    fetchEvent.respondWith(
+      caches.match(fetchEvent.request).then(res => {
+        return res || fetch(fetchEvent.request)
+      })
+    )
+  })

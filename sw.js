567986a9a0efce528dc0a94cc49c171ab95cb171
Barthezzone1 <s219117@student.uek.krakow.pl>
2023-03-27 17:56:18 +0200

    pwa powrót?

--- sw.js
+++ sw.js
@@ -24,3 +24,13 @@ self.addEventListener("fetch", fetchEvent => {
       })
     )
   })
+
+  window.addEventListener('load', function() {
+    window.history.pushState({ noBackExitsApp: true }, '')
+  })
+  
+  window.addEventListener('popstate', function(event) {
+    if (event.state && event.state.noBackExitsApp) {
+      window.history.pushState({ noBackExitsApp: true }, '')
+    }
+  })
